09ea8670106c23e43623ecb1e6d5afaa4c740f57
cwong165 <77028662+cwong165@users.noreply.github.com>
2023-03-10 00:02:32 -0800

    Create main.py
    This is a basic structure of the code that we are going to implement
    We would not use the built-in method in numpy to do Bounded Leplace Algorithm, instead we would implement it by using only math and random libraries
    This program expects a COVID dataset which you can find in our GitHub link. The dataset was originally found on Kaggle.

---
+++ main.py
@@ -0,0 +1,33 @@
+import csv
+import random
+import math
+
+# =====================================================================================================================================================
+# This is a basic structure of the code that we are going to implement
+# We would not use the built-in method in numpy to do Bounded Leplace Algorithm, instead we would implement it by using only math and random libraries
+# This program expects a COVID dataset which you can find in our GitHub link. The dataset was originally found on Kaggle.
+# =====================================================================================================================================================
+
+
+# ============Data Handling=================
+# Load dataset
+# CODE HERE
+# Do Data Cleaning
+# CODE HERE
+# =================END=======================
+
+
+# =======Algorithem Implementation===========
+# Define an Algorithm that finds Epsilon
+# CODE HERE
+
+# Define Bounded Leplace Algorithm 
+# CODE HERE
+# ==============END=========================
+
+
+
+
+# ======Apply Alogirhtem to DataSet========
+# CODE HERE
+# =============END=========================
